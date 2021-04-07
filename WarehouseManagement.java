4977ad9260a63e0cd7750e512905350cdc4f3e0d
Andrew Mahr <63127691+acompsec1@users.noreply.github.com>
2021-04-07 18:31:57 -0400

    Andrew Setting up his code

---
+++ WarehouseManagement.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class WarehouseManagement extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Login_screen.fxml"));
+
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+}
